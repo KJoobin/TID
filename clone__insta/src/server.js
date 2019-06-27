8de443cb5973540c703cf72858af6cda90ab52c6
Kjoobin <imkj1148@gmail.com>
2019-06-27 16:05:02 +0900

    Add morgan ( logger )

--- clone__insta/src/server.js
+++ clone__insta/src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 import { GraphQLServer } from "graphql-yoga"
+import logger from "morgan"
 
 const PORT = process.env.PORT || 4000;
 const typeDefs = `
@@ -15,5 +16,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer( { typeDefs, resolvers})
+
+server.express.use(logger("dev"))
 
 server.start({port : PORT}, () => console.log(`Server running ${PORT}`))
