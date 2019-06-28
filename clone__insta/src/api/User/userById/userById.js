ab00d66ad81fde8162f86c51f87ff5b734255be1
Kjoobin <imkj1148@gmail.com>
2019-06-28 12:16:25 +0900

    Connect prisma - graphQL and add test case

---
+++ clone__insta/src/api/User/userById/userById.js
@@ -0,0 +1,10 @@
+import { prisma } from '../../../../generated/prisma-client'
+
+export default {
+  Query : {
+    userById : async (_, arg) => {
+      const { id } = arg
+      return await prisma.user({ id })
+    }
+  }
+}
