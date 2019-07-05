ec168acaa94aa29e856c343de2e8d8e90e073752
Kjoobin <imkj1148@gmail.com>
2019-07-05 19:43:58 +0900

    Find err but don't know why?

--- clone__insta/src/server.js
+++ clone__insta/src/server.js
@@ -1,18 +1,20 @@
-import dotenv from "dotenv"
-import path from "path"
-dotenv.config({path:path.join(__dirname,'.env')})
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
-import { GraphQLServer } from "graphql-yoga"
-import logger from "morgan"
-import schema from "./schema"
-import { sendSecretMail } from "./utils"
-
-
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+import { sendSecretMail } from "./utils";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer( { schema})
+const server = new GraphQLServer({ schema });
 
-server.express.use(logger("dev"))
+server.express.use(logger("dev"));
 
-server.start({port : PORT}, () => console.log(`Server running ${PORT}`))
+server.start({ port : PORT }, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
+);
+// port : PORT 하면 alredy use err;
+// port : 4000 --> run why?
