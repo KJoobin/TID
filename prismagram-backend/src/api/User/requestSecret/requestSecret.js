976fdb2fe4e38a724a768444273b7693849bdbc6
Kjoobin <imkj1148@gmail.com>
2019-07-13 23:22:15 +0900

    Fixed return value boolean to [exist login, exist login secret]

--- prismagram-backend/src/api/User/requestSecret/requestSecret.js
+++ prismagram-backend/src/api/User/requestSecret/requestSecret.js
@@ -3,19 +3,24 @@ import { prisma } from "../../../../generated/prisma-client"
 
 export default {
   Mutation: {
-    requestSecret: async(_, args, { request }) => {
-      console.log(request.user)
+    requestSecret: async (_, args, { request }) => {
+      console.log(args)
       const { email } = args;
+      const [existUser, ...rest] = await prisma.users({ where:{ email }})
       const loginSecret = await secretGenerator();
-      try {
-        throw Error();
-         await sendSecretMail( email, loginSecret)
-         await prisma.updateUser({data:{ loginSecret },where:{ email }});
-         return true;
-      } catch(err){
-        console.log(err)
-        return false;
-      }
+            try {
+              if(existUser.loginSecret) {
+                return ["true","true"]
+              } else {
+                // throw Error("login secret is Exist");
+                await sendSecretMail( email, loginSecret)
+                await prisma.updateUser({data:{ loginSecret },where:{ email }});
+                return ["true","false"]
+             }
+            } catch(err){
+              console.log(err.message)
+              return ["false"];
+            }
     }
   }
 }
