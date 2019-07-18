00f2ba049d50679318ad011efa27bf84f8a83dd7
Kjoobin <imkj1148@gmail.com>
2019-07-18 09:19:10 +0900

    Remove change login secret when after login

--- prismagram-backend/src/api/User/confirmSecret/confirmSecret.js
+++ prismagram-backend/src/api/User/confirmSecret/confirmSecret.js
@@ -7,10 +7,6 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if(user.loginSecret === secret){
-        await prisma.updateUser({
-          where:{ email },
-          // data:{ loginSecret: "" }
-        })
         return generateToken(user.id);
       } else {
          throw Error("Wrong eamil/secret combination ")
